a59cd9db44ab05216bc3e20c1a5425e5465fe428
Vidkoo <vidkode@gmail.com>
2023-11-06 17:17:31 +0100

    Ajout du footer dans un fichier footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <h2>Suivez-nous</h2>
+    <div class="social">
+        <a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+        <a href="https://www.x.com/" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+    <p>Copyright &copy; <?php echo date("Y"); ?> - <a href="#" id="CGU">Conditions générales d'utilisation</a></p>
+</footer>
