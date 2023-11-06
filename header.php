47d4863325e0703c6cb6decdb39b0d1456403c03
Vidkoo <vidkode@gmail.com>
2023-11-06 17:18:04 +0100

    Ajout du header dans un fichier header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+    <img src="img/Logo_redactilibre.png" alt="Logo avec un livre et des stylos">
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html"><i class="fa-solid fa-house"></i> Accueil</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-right-to-bracket"></i> Inscription</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-user"></i> Connexion</a>
+            </li>
+        </ul>
+    </nav>
+</header>
