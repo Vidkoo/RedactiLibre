5c2b2b9e9543b98285a5da935315f33c65130ef3
Vidkoo <vidkode@gmail.com>
2023-11-13 17:12:11 +0100

    Modification du formulaire d'inscription

--- sign_up.php
+++ sign_up.php
@@ -15,7 +15,7 @@
         <title>RédactiLibre | Inscription</title>
         <script src="https://kit.fontawesome.com/4fd84f8514.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/sign_up.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
@@ -28,24 +28,20 @@
         <!-- Formulaire d'inscription -->
         <main>
             <form action="" method="post">
-                <div>
-                    <label for="pseudo">Pseudonyme <input type="text" name="pseudo" id="pseudo"></label>
-                </div>
-                <div>
-                    <label for="mail">Adresse mail <input type="email" name="mail" id="mail"></label>
-                </div>
-                <div>
-                    <label for="password">Mot de passe <input type="password" name="password" id="password"></label>
-                </div>
-                <div>
-                    <label for="birth-day">Votre date de naissence <input type="date" name="birth-day" id="birth-day"></label>
-                </div>
-                <div>
-                    <label for="CGU"><input type="checkbox" name="cgu_agree" id="CGU"> Accepter les conditions générales d'utilisation</label>
-                </div>
-                <div>
-                    <input type="button" value="S'inscrire">
-                </div>
+                <fieldset>
+                    <h2>Inscription</h2>
+                        <label for="pseudo">Pseudonyme <input type="text" name="pseudo" id="pseudo"></label>
+                        <br>
+                        <label for="mail">Adresse mail <input type="email" name="mail" id="mail"></label>
+                        <br>
+                        <label for="password">Mot de passe <input type="password" name="password" id="password"></label>
+                        <br>
+                        <label for="birth-day">Votre date de naissence <input type="date" name="birth-day" id="birth-day"></label>
+                        <br>
+                        <label for="CGU"><input type="checkbox" name="cgu_agree" id="CGU"> Accepter les conditions générales d'utilisation</label>
+                        <br>
+                        <input type="button" value="S'inscrire">
+                </fieldset>
             </form>
         </main>
 
